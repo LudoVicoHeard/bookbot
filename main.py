a801fd0dbbbbf02f961f02ed022e2afc014a03e3
Lewis Heard <lewisheard@hotmail.com>
2025-03-04 20:29:40 +0000

    Finished

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
-from stats import char_counts, word_count
+import sys
+from stats import char_counts, word_count, sorted_alpha_chars
 
 def main():
-    path = "books/frankenstein.txt"
+
+    if sys.argv.count != 2:
+        print("Usage: python3 main.py <path_to_book>")
+    
+
+    path = sys.argv[1]
 
     book_text = get_book_text(path)
     wc = word_count(book_text)
     char_list_and_count = char_counts(book_text)
+    sorted_alpha_output = sorted_alpha_chars(char_list_and_count)
+    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {wc} total words")
+    print("--------- Character Count -------")
+    for item in sorted_alpha_output:
+        print(f"{item[0]}: {item[1]}")
+    print("============= END ===============")
 
-    print(f"{wc} words found in the document")
-    print(f"{char_list_and_count}")
+
+    #print(f"{wc} words found in the document")
+    #print(sorted_alpha_output)
 
 def get_book_text(book):
     with open(book) as f:
         text = f.read()
     return text
+
     
 main()
 
--- stats.py
+++ stats.py
@@ -6,8 +6,13 @@ def char_counts(text):
     char_list = list(text.lower())
     char_dict = {}
     for char in char_list:
-        if char in char_dict:
-            char_dict[char] += 1
-        else:
-            char_dict[char] = 1
+        if char.isalpha():  # Only count alphabetic characters
+            if char in char_dict:
+                char_dict[char] += 1
+            else:
+                char_dict[char] = 1
     return char_dict
+
+def sorted_alpha_chars(input):
+    sorted_chars = sorted(input.items(), key=lambda x: x[1], reverse=True)
+    return sorted_chars
